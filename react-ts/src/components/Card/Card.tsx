4bdc25aa758049b3b601e0b2f54c392e3ca1ee10
Malu Abrami <marialuizaabrami@gmail.com>
2025-12-09 16:38:28 -0300

    fix: ajustando pagina de lançamentos

--- react-ts/src/components/Card/Card.tsx
+++ react-ts/src/components/Card/Card.tsx
@@ -13,9 +13,7 @@ export function Card({ children }: CardProps) {
             borderRadius: "8px",
             boxShadow: "0px 2px 8px rgba(0,0,0,0.1)",
             marginBottom: "20px",
-            width: "30%",
-            justifyContent: "center",
-            textAlign: "center"
+            width: "100%",
         }}
         >
         {children}
--- react-ts/src/pages/EntriesPage.tsx
+++ react-ts/src/pages/EntriesPage.tsx
@@ -6,24 +6,25 @@ import { Link } from "react-router-dom";
 import { financialEntryApi } from "../api/financialEntryApi";
 import { useEffect, useState } from "react";
 import type { GetAllFinancialEntrysByUser } from "../types/GetAllFinancialEntrysByUser";
+import styles from "./EntriesPage.module.css";
 
 export function EntriesPage() {
-    const [entries, setEntries] = useState<GetAllFinancialEntrysByUser[]>([]);
+  const [entries, setEntries] = useState<GetAllFinancialEntrysByUser[]>([]);
 
-    useEffect(() => {
+  useEffect(() => {
     financialEntryApi.list().then((res) => {
-        const list = res.data.financialsByUser.map((entry) => ({
+      const list = res.data.financialsByUser.map((entry) => ({
         ...entry,
         type: entry.type == 1 ? "Gasto" : "Ganho",
         isFixed: entry.isFixed ? "Sim" : "Não",
-        date: entry.date.split("T")[0] // opcional para formatar
-        }));
+        date: entry.date.split("T")[0], // opcional para formatar
+      }));
 
-        setEntries(list);
+      setEntries(list);
     });
-    }, []);
+  }, []);
 
-    const columns = [
+  const columns = [
     { label: "Identificador", key: "id" },
     { label: "Valor", key: "amount" },
     { label: "Categoria", key: "category" },
@@ -31,19 +32,20 @@ export function EntriesPage() {
     { label: "Data", key: "date" },
     { label: "Tipo", key: "type" },
     { label: "Fixo", key: "isFixed" },
-    ];
+  ];
 
-    return (
-        <MainLayout>
-            <h1>Lançamentos</h1>
+  return (
+    <MainLayout>
+      <div className={styles.topContainer}>
+        <h1>Lançamentos</h1>
+        <Link to={"/entries/new"}>
+          <Button label="Novo lançamento" />
+        </Link>
+      </div>
 
-            <Card>
-                <Table columns={columns} data={entries}></Table>
-            </Card>
-
-            <Link to={"/entries/new"}>
-                <Button label="Novo lançamento"/>
-            </Link>
-        </MainLayout>
-    );
+      <Card>
+        <Table columns={columns} data={entries}></Table>
+      </Card>
+    </MainLayout>
+  );
 }
