dbf792a7dca02779ed5e0b6b5de4b44d7456c79a
Malu Abrami <marialuizaabrami@gmail.com>
2025-12-11 11:19:53 -0300

    fix: ajustando card e pagina de perfil

--- react-ts/src/components/Card/Card.tsx
+++ react-ts/src/components/Card/Card.tsx
@@ -12,8 +12,8 @@ export function Card({ children }: CardProps) {
             padding: "20px",
             borderRadius: "8px",
             boxShadow: "0px 2px 8px rgba(0,0,0,0.1)",
-            marginBottom: "20px",
             width: "100%",
+            height: "92vh"
         }}
         >
         {children}
--- react-ts/src/pages/UserPage.tsx
+++ react-ts/src/pages/UserPage.tsx
@@ -6,21 +6,26 @@ import styles from "./UserPage.module.css";
 import { AuthContext } from "../utils/AuthContext";
 
 export function UserPage() {
+    const email = localStorage.getItem("userCompanyEmail");
     const {logout} = useContext(AuthContext);
 
     return (
         <MainLayout>
             <Card>
-                <div className={styles.imageContainer}>
-                    <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
-                </div>
-                <div className={styles.infosContainer}>
-                    <p>Email: </p>
-                    <p>Senha: *******</p>
-                </div>
-                <div className={styles.buttonsContainer}>
-                    <Button label="Atualizar" onClick={() => alert("em breve")}></Button>
-                    <Button label="Sair" onClick={() => logout()}></Button>
+                <div className={styles.container}>
+                    <div className={styles.userContainer}>
+                        <div className={styles.imageContainer}>
+                            <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
+                        </div>
+                        <div className={styles.infosContainer}>
+                            <p>Email: {email}</p>
+                            <p>Senha: *******</p>
+                        </div>
+                    </div>
+                    <div className={styles.buttonsContainer}>
+                        <Button label="Atualizar" onClick={() => alert("em breve")}></Button>
+                        <Button label="Sair" onClick={() => logout()}></Button>
+                    </div>
                 </div>
             </Card>
         </MainLayout>
